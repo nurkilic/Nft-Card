c0248b8c12e154d2bcae48e31aec3565eb3a7540
Nur <rumeysanurkilic@gmail.com>
2024-11-01 16:30:27 +0300

    initial responsive settings adjusted

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  return (
+
+      <div className="flex justify-center items-center min-h-screen">
+        <Card />
+      </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <>
+      <div className="card bg-gray-200 p-2 rounded-md mx-3 min-h-72"> </div>
+    </>
+  );
+};
+
+export default Card;
